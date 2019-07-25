b9b2f5eec2001550e16268ea067547a65a5f03ef
Rory Murray <rory.christian.murray@gmail.com>
2019-07-25 15:25:06 -0400

    create store, setup reducer

---
+++ crypto-list/src/reducers/index.js
@@ -0,0 +1,14 @@
+// Action types
+
+// Initial state
+
+const initialState = {};
+
+// Reducer
+
+export const rooterReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    default:
+      return state;
+  }
+};
